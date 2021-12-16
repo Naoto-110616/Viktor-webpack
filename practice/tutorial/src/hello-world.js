7bb45482548040d5cb7e921478bfb5b27318809c
Naoto-110616 <siodeitadaku@gmail.com>
2021-12-16 13:43:25 +0900

    multiple bundles

--- practice/tutorial/src/hello-world.js
+++ practice/tutorial/src/hello-world.js
@@ -1,5 +1,15 @@
-const helloWorld = () => {
-	console.log("Hello world!");
-};
+import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
 
-export default helloWorld;
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
+if (process.env.NODE_ENV === "production") {
+	console.log("production mode");
+} else if (process.env.NODE_ENV === "development") {
+	console.log("development mode");
+}
+
+helloWorldButton.methodThatDoesNotExit();
---
+++ practice/tutorial/src/img.js
@@ -0,0 +1,7 @@
+import Heading from "./components/heading/heading";
+import ExampleImage from "./components/example-image/example-image";
+
+const heading = new Heading();
+heading.render();
+const exampleImage = new ExampleImage();
+exampleImage.render();
